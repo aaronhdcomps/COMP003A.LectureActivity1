ae0669e3860090341a25ad50edf2e14d163bff98
Aaron Abramson <aaronabramson@my.whccd.edu>
2023-01-18 19:14:55 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,18 @@
+﻿//  this is a single-line comment
+ /*  this is a multi-line comment
+  *  Author: Aaron Abramson
+  *  Course: COMP-003A
+  *  Purpose: Lecture activity for week 1
+  */
+ namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("this is a string");
+            Console.WriteLine("Aaron Abramson");
+        }
+    }
+}
